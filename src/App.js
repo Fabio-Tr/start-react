fefd3ff5afee21cb383b68f0091a3cafb2bcc49e
Fabio Trani <trani.fabio.90@gmail.com>
2022-01-31 13:37:31 +0100

    card test

--- src/App.js
+++ src/App.js
@@ -4,9 +4,22 @@ import Button from './components/Button';
 import React, { Component, useState } from 'react';
 import Navbar from './components/Navbar';
 import Card from './components/Card';
+import img1 from './img/1.jpg'
+import img2 from './img/2.jpg'
+import img3 from './img/3.jpg'
+import img4 from './img/4.jpg'
 
 
 class App extends Component {
+  state = {
+    cards: [
+      { id: 0, title: "tizio", img: img1, descrption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
+      { id: 1, title: "caio", img: img2, descrption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
+      { id: 2, title: "sempronio", img: img3, descrption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
+      { id: 3, title: "ermenegildo", img: img4, descrption: "Lorem Ipsum is simply dummy text of the printing and typesetting industry."},
+    ]
+
+  }
   render() {
 
     return (
@@ -29,7 +42,14 @@ class App extends Component {
           <Button text='Clicca su' />
           <div className='container'>
             <div className='row'>
-              <Card />
+              {this.state.cards.map(card => (
+                <Card
+                  key={card.id}
+                  title={card.title}
+                  description={card.descrption}
+                  img={card.img}
+                />
+              ))}
             </div>
           </div>
         </header>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,4 @@
 import react, { Component } from "react";
-import img from '../logo3.png';
 
 
 class Card extends Component {
@@ -7,10 +6,10 @@ class Card extends Component {
         return (
             <div className="col">
                 <div className="card" style={{ width: "18rem", textAlign: "center" }}>
-                    <img src={img} className="card-img-top" />
+                    <img src={this.props.img} className="card-img-top" />
                     <div className="card-body">
-                        <h5 className="card-title">Card title</h5>
-                        <p className="card-text text-dark">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <h5 className="card-title text-dark">{this.props.title}</h5>
+                        <p className="card-text text-dark">{this.props.description}</p>
                         <a href="#" className="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
